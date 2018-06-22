e124521372f0ae89a515a1f461ba9295fb9ef57f
Arthur Nishikawa <nishikawa.poli@gmail.com>
2018-06-22 01:06:06 -0300

    Tweaking iteration parameters

--- coupled_FoFo_375_CCEortho.py
+++ coupled_FoFo_375_CCEortho.py
@@ -13,7 +13,7 @@ basename = os.path.basename(__file__).replace('.py', '')
 c0 = 3.34414e-02
 T_C = 375.
 
-control_itsteps = ControlIterationSteps([5e-4, 5e-3, 5e-2], [0, 1, 100, 1000])
+control_itsteps = ControlIterationSteps([5e-4, 5e-3, 5e-2], [0, 1, 150, 1000])
 total_time = control_itsteps.total_time
 n_time = control_itsteps.ntime
 dt = control_itsteps.dt
