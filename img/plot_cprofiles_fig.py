00c617d3ec119fc685088a2c848fdcd19e451f2e
Arthur Nishikawa <nishikawa.poli@gmail.com>
2018-10-18 14:26:40 +0200

    Change fontsize, add phase labeling

--- img/plot_cprofiles_fig.py
+++ img/plot_cprofiles_fig.py
@@ -38,6 +38,13 @@ if __name__ == '__main__':
     WBs = 1.5902
 
     ############
+    labels = [('aus1', r'$\gamma$', 1),
+              ('aus2', r'$\gamma$', 1),
+              ('aust', r'$\gamma$', 1),
+              ('fer1', r'$\alpha_{b}$', 1),
+              ('fer2', r'$\alpha_{b}$', 1),
+              ('mart', r"$\alpha' + \theta$", 1)]
+
     p1 = CProfiles('coupled_FoFo_375_CCEortho', '../C_profiles')
 
     ax1.axhline(c0, ls=':', color='k', lw=1)
@@ -47,12 +54,18 @@ if __name__ == '__main__':
 
     ax1.set_xlim(-1.16, 1.16)
     ax1.set_ylim(-.02, 1.9)
-    ax1.text(.98, .98, r"$\alpha'-\theta$" + ' ortho',
-             ha='right', va='top', transform=ax1.transAxes)
+    ax1.text(.95, .95, r"$\alpha'-\theta$" + ' ortho',
+             ha='right', va='top', size=10,
+             transform=ax1.transAxes,
+             backgroundcolor='white')
     add_label(ax1, 'a)', py=0)
     ax1.set_xlabel(u'Position (μm)')
     ax1.set_ylabel('Carbon content (wt.%)')
-    ax1.legend(loc='upper left', fancybox=False)
+    ax1.legend(loc='lower right', fancybox=False, fontsize=10)
+
+    p1.label_phases(ax=ax1, t=1,
+                    labels=labels,
+                    mirror=True, size=12)
 
     ############
     p2 = CProfiles('coupled_FoFo_375_CCEpara', '../C_profiles')
@@ -63,13 +76,19 @@ if __name__ == '__main__':
                       mirror=True, lw=1)
 
     ax2.set_xlim(-1.16, 1.16)
-    ax2.set_ylim(-.05, 4.2)
-    ax2.text(.98, .98, r"$\alpha'-\theta$" + ' para',
-             ha='right', va='top', transform=ax2.transAxes)
+    ax2.set_ylim(-.05, 4.3)
+    ax2.text(.95, .95, r"$\alpha'-\theta$" + ' para',
+             ha='right', va='top', size=10,
+             transform=ax2.transAxes,
+             backgroundcolor='white')
     add_label(ax2, 'b)', py=0)
     ax2.set_xlabel(u'Position (μm)')
     ax2.set_ylabel('Carbon content (wt.%)')
-    ax2.legend(loc='upper left', fancybox=False)
+    ax2.legend(loc='lower right', fancybox=False, fontsize=10)
+
+    p2.label_phases(ax=ax2, t=1,
+                    labels=labels,
+                    mirror=True, size=12)
 
     ############
     p3 = CProfiles('coupled_FoFo_375_mu23e3', '../C_profiles')
@@ -81,16 +100,20 @@ if __name__ == '__main__':
 
     ax3.set_xlim(-1.16, 1.16)
     ax3.set_ylim(-.02, 1.9)
-    ax3.text(.98, .98, r"$\mu_C=$" + '23.2 kJ/mol' + r' ($c^\gamma_{int}$ = WBs)',
-             ha='right', va='top', transform=ax3.transAxes)
+    ax3.text(.95, .95, r"$\mu_C=$" + '23.2 kJ/mol' + r' ($c^\gamma_{int}$ = WBs)',
+             ha='right', va='top', size=10,
+             transform=ax3.transAxes,
+             backgroundcolor='white')
     add_label(ax3, 'c)', py=0)
     ax3.set_xlabel(u'Position (μm)')
     ax3.set_ylabel('Carbon content (wt.%)')
-    ax3.legend(loc='upper left', fancybox=False)
+    ax3.legend(loc='lower right', fancybox=False, fontsize=10)
 
-    """
-    Plot average C composition
-    """
+    p3.label_phases(ax=ax3, t=1,
+                    labels=labels,
+                    mirror=True, size=12)
+
+    # Plot average C composition
 
     files = ['../C_avg/coupled_FoFo_375_CCEortho.txt',
              '../C_avg/coupled_FoFo_375_mu20e3.txt',
@@ -107,7 +130,7 @@ if __name__ == '__main__':
     ax4.axhline(c0, ls=':', color='k', lw=1)
     ax4.axhline(WBs, ls=':', color='k', lw=1)
     add_label(ax4, 'd)', py=0)
-    ax4.legend(loc='lower right', fancybox=False)
+    ax4.legend(loc='lower right', fancybox=False, fontsize=10)
 
     fout = 'cpartition.svg'
     fig.savefig(fout, bbox_inches='tight')
